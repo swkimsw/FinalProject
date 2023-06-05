7f7cbef3d39f0c00d834b6b4d4c27424c7851765
swkimsw <swkimsw3746@gmail.com>
2023-06-05 15:47:38 +0900

    kim

---
+++ src/main/java/cc/spring/controllers/ClientMemberController.java
@@ -0,0 +1,13 @@
+package cc.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/clientMember/")
+public class ClientMemberController {
+
+
+
+}
