6e598d446eb8d4ebb36c9c6e64a68d9d8b1f413d
Uoioio <joy10078@gmail.com>
2023-06-05 17:13:32 +0900

    5pm 13

---
+++ src/main/java/cc/spring/controllers/BoardController.java
@@ -0,0 +1,18 @@
+package cc.spring.controllers;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/board/")
+public class BoardController {
+
+	@RequestMapping("list")
+	public String home() {
+		return "/board/boardFree/";
+	}
+}
