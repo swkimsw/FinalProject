bad4a84ea488caade439c82932ae4ebc45ca52bf
agji12 <a@b>
2023-06-09 09:07:37 +0900

    shop update

--- src/main/java/cc/spring/services/ShopService.java
+++ src/main/java/cc/spring/services/ShopService.java
@@ -26,7 +26,7 @@ public class ShopService {
 	private FileDAO fileDAO;
 	
 	@Transactional
-	public int insertShop(ShopDTO dto, MultipartFile[] files, String realPath) throws Exception {
+	public void insertShop(ShopDTO dto, MultipartFile[] files, String realPath) throws Exception {
 		int parentSeq = 0;
 		
 		// shop 정보 insert
@@ -38,26 +38,20 @@ public class ShopService {
 			dto.setDeadLine(timestamp);
 			
 			parentSeq = shopDAO.insertShop(dto);
-			System.out.println("부모 시퀀스 : " + parentSeq);
 		}
 		
 		// shop image insert
 		File realPathFile = new File(realPath);
 		if(!realPathFile.exists()) realPathFile.mkdir();
-		System.out.println("파일 !! : " + files);
 		if(files != null) {
-			System.out.println("파일들 : " + files);
 			for(MultipartFile file : files) {
 				if(file.isEmpty()) {break;}
 				String oriName = file.getOriginalFilename();
 				String sysName = UUID.randomUUID() + "_" + oriName;
 				file.transferTo(new File(realPath+"/"+sysName));
-				return fileDAO.insertShopImage(new FileDTO(0, parentSeq, realPath,oriName, sysName));
+				fileDAO.insertShopImage(new FileDTO(0, parentSeq, realPath,oriName, sysName));
 			}
 		}
-		
-		
-		return 0;
 	}
 	
 }
