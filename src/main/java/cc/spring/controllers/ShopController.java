1c30939e9c876170d121e50ec0aea314cc725003
agji12 <a@b>
2023-06-09 17:16:18 +0900

    shop update

--- src/main/java/cc/spring/controllers/ShopController.java
+++ src/main/java/cc/spring/controllers/ShopController.java
@@ -4,9 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import cc.spring.dto.ShopDTO;
@@ -21,25 +21,30 @@ public class ShopController {
 
 	@Autowired
 	private HttpSession session;
-
+	
+	// 공구샵 등록 폼으로 이동
 	@RequestMapping("toShopRegister")
 	public String toShopRegister() {
 		return "/shop/shopRegister";
 	}
-
-	@RequestMapping("toShopList")
-	public String toShopList() {
-		return "/shop/shopList";
+	
+	// 공구샵 신청 폼으로 이동
+	@RequestMapping("toShopApply")
+	public String toShopApply(int code, Model model) {
+		ShopDTO shopDTO = shopService.selectShopInfo(code);
+		model.addAttribute("shopDTO", shopDTO);
+		return "/shop/shopApply";
 	}
 
-	@RequestMapping(value="insertShop")
+	// 공구샵 등록시 DB에 insert
+	@RequestMapping("insertShop")
 	public String insertShop(ShopDTO dto, MultipartFile[] files) throws Exception {
 
 		// realPath - 폴더가 없다면 만들기
 		String realPath = session.getServletContext().getRealPath("upload");
 		shopService.insertShop(dto, files, realPath);
 
-		return "redirect:/shop/toShopRegister";
+		return "redirect:/";
 	}
 
 	@ExceptionHandler(Exception.class)
