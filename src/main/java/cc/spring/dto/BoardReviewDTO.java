18f895a43ddc97e14a2b67a6481cb1e80e9784e0
Uoioio <joy10078@gmail.com>
2023-06-12 16:04:35 +0900

    dto , controller , service , dao, mapper등등 생성

---
+++ src/main/java/cc/spring/dto/BoardReviewDTO.java
@@ -0,0 +1,99 @@
+package cc.spring.dto;
+
+import java.sql.Timestamp;
+
+public class BoardReviewDTO {
+	private int code;
+	private int boardkindcode;
+	private int headlinecode;
+	private int clientcode;
+	private String title;
+	private String content;
+	private int viewcount;
+	private int likecout;
+	private Timestamp regdate;
+	private Timestamp moddate;
+	private Timestamp deldate;
+	public BoardReviewDTO() {} ;
+	public BoardReviewDTO(int code, int boardkindcode, int headlinecode, int clientcode, String title, String content,
+			int viewcount, int likecout, Timestamp regdate, Timestamp moddate, Timestamp deldate) {
+		super();
+		this.code = code;
+		this.boardkindcode = boardkindcode;
+		this.headlinecode = headlinecode;
+		this.clientcode = clientcode;
+		this.title = title;
+		this.content = content;
+		this.viewcount = viewcount;
+		this.likecout = likecout;
+		this.regdate = regdate;
+		this.moddate = moddate;
+		this.deldate = deldate;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public int getBoardkindcode() {
+		return boardkindcode;
+	}
+	public void setBoardkindcode(int boardkindcode) {
+		this.boardkindcode = boardkindcode;
+	}
+	public int getHeadlinecode() {
+		return headlinecode;
+	}
+	public void setHeadlinecode(int headlinecode) {
+		this.headlinecode = headlinecode;
+	}
+	public int getClientcode() {
+		return clientcode;
+	}
+	public void setClientcode(int clientcode) {
+		this.clientcode = clientcode;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getViewcount() {
+		return viewcount;
+	}
+	public void setViewcount(int viewcount) {
+		this.viewcount = viewcount;
+	}
+	public int getLikecout() {
+		return likecout;
+	}
+	public void setLikecout(int likecout) {
+		this.likecout = likecout;
+	}
+	public Timestamp getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Timestamp regdate) {
+		this.regdate = regdate;
+	}
+	public Timestamp getModdate() {
+		return moddate;
+	}
+	public void setModdate(Timestamp moddate) {
+		this.moddate = moddate;
+	}
+	public Timestamp getDeldate() {
+		return deldate;
+	}
+	public void setDeldate(Timestamp deldate) {
+		this.deldate = deldate;
+	}
+}
---
+++ src/main/java/cc/spring/repositories/BoardDAO.java
@@ -0,0 +1,35 @@
+package cc.spring.repositories;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import cc.spring.dto.BoardFreeDTO;
+import cc.spring.dto.BoardReviewDTO;
+
+@Repository
+public class BoardDAO {
+
+	
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	public int selectReviewSeq() {
+		return mybatis.selectOne("Board.selectReviewSeq");
+	}
+	
+	public int insertReview(BoardReviewDTO  dto,String writer,int parent_seq) {
+		Map<String ,Object> param = new HashMap<>();
+		param.put("dto", dto);
+		param.put("writer", writer);
+		param.put("seq", parent_seq);
+		
+		return mybatis.insert("Board.insertReview",param);
+	}
+
+
+
+}
