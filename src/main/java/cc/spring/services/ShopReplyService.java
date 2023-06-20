8568d8734857e1906a934c559d8fde538a38396e
agji12 <a@b>
2023-06-21 00:46:16 +0900

    ShopReply Update

--- src/main/java/cc/spring/services/ShopReplyService.java
+++ src/main/java/cc/spring/services/ShopReplyService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cc.spring.dto.ShopReplyAnswerDTO;
 import cc.spring.dto.ShopReplyAskDTO;
 import cc.spring.repositories.ShopReplyDAO;
 
@@ -32,6 +33,11 @@ public class ShopReplyService {
 	// 댓글 delete
 	public int deleteReplyAsk(ShopReplyAskDTO dto) {
 		return shopReplyDAO.deleteReplyAsk(dto);
+	}
+	
+	// 답글 insert
+	public int insertReplyAnswer(ShopReplyAnswerDTO dto) {
+		return shopReplyDAO.insertReplyAnswer(dto);
 	}
 	
 }
