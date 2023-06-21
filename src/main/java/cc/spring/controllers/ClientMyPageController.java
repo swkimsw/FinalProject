6618e681b8b2a665946db46b34c99bec6bc54134
soobinS2 <ysb930901@gmail.com>
2023-06-21 17:43:29 +0900

    DDOS

--- src/main/java/cc/spring/controllers/ClientMyPageController.java
+++ src/main/java/cc/spring/controllers/ClientMyPageController.java
@@ -1,17 +1,18 @@
 package cc.spring.controllers;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.maven.settings.Settings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import cc.spring.dto.BoardFreeDTO;
-import cc.spring.repositories.ClientMyPageDAO;
 import cc.spring.services.ClientMyPageService;
 
 @Controller
@@ -22,7 +23,6 @@ public class ClientMyPageController {
 
 	@Autowired
 	private ClientMyPageService cmp;
-
 
 	@RequestMapping("myPage")
 	public String myPageList(Model model) {
@@ -36,6 +36,43 @@ public class ClientMyPageController {
 		return "/member/myPage";
 	}
 	
+    @RequestMapping("/study_select.adminBoard")
+    public String clientFreeBoard(HttpServletRequest request, Model model) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        int currentPage = Integer.parseInt(request.getParameter("cpage"));
+        System.out.println("currentPage: " + currentPage);
+
+        // 페이지 번호 검증
+        int totalPage = (int) Math.ceil(cmd.getRecordCount() / (double) Settings.BOARD_RECORD_COUNT_PER_PAGE);
+        if (currentPage < 0) {
+            currentPage = 1;
+        } else if (currentPage > totalPage) {
+            currentPage = totalPage;
+        }
+
+        int start = currentPage * Settings.BOARD_RECORD_COUNT_PER_PAGE - (Settings.BOARD_RECORD_COUNT_PER_PAGE - 1);
+        int end = currentPage * Settings.BOARD_RECORD_COUNT_PER_PAGE;
+
+        int first = (currentPage - 1) / Settings.BOARD_NAVI_COUNT_PER_PAGE * Settings.BOARD_NAVI_COUNT_PER_PAGE;
+        int last = (currentPage - 1) / Settings.BOARD_NAVI_COUNT_PER_PAGE * Settings.BOARD_NAVI_COUNT_PER_PAGE + Settings.BOARD_NAVI_COUNT_PER_PAGE + 1;
+
+        System.out.println("start/end: " + start + "/" + end);
+        System.out.println("first/last: " + first + "/" + last);
+        System.out.println("currentPage: " + currentPage);
+
+        List<BoardFreeDTO> list = cmd.selectStudyBoard(start, end);
+        List<String> pageNavi = cmd.getPageNavi(cmd.getRecordCount(), currentPage);
+
+        System.out.println("pageNavi: " + pageNavi);
+
+        model.addAttribute("list", list);
+        model.addAttribute("navi", pageNavi);
+        model.addAttribute("cpage", currentPage);
+        model.addAttribute("start", first);
+        model.addAttribute("end", last);
+
+        return "/admin/admin_StudyBoard";
+    }
 	// 내 정보 보기 클릭 시 페이지 이동
 	@RequestMapping("myInfo")
 	public String myInfo() {
--- src/main/java/cc/spring/services/ClientMyPageService.java
+++ src/main/java/cc/spring/services/ClientMyPageService.java
@@ -1,5 +1,6 @@
 package cc.spring.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,74 @@ public class ClientMyPageService {
 		System.out.println("죽고잡냐");
 		return cmdao.selectCode(code);
 	
+	
 }
+//	잠시 대기 하단페이징
+	public List<String> getPageNavi(int recordCount, int currentPage) throws Exception {
+		int recordTotalCount = recordCount;
+		int recordCountPerPage = 10;
+//				Settings.BOARD_RECORD_COUNT_PER_PAGE;
+		int naviCountPerPage = 10;
+//				Settings.BOARD_NAVI_COUNT_PER_PAGE;
+
+		int pageTotalCount = (int) Math.ceil(recordTotalCount / (double) recordCountPerPage);
+
+		if (currentPage < 1) {
+			currentPage = 1;
+		} else if (currentPage > pageTotalCount) {
+			currentPage = pageTotalCount;
+		}
+		int startNavi = ((currentPage - 1) / naviCountPerPage) * naviCountPerPage + 1;
+		int endNavi = startNavi + (naviCountPerPage - 1);
+		if (endNavi > pageTotalCount) {
+			endNavi = pageTotalCount;
+		}
+		boolean needPagePrev = true;
+		boolean needPageNext = true;
+		boolean needPrev = true;
+		boolean needNext = true;
+		if (startNavi == 1) {
+			needPagePrev = false;
+		}
+		if (endNavi == pageTotalCount) {
+			needPageNext = false;
+		}
+		if (currentPage == pageTotalCount) {
+			needNext = false;
+		}
+		if (currentPage == 1) {
+			needPrev = false;
+		}
+		List<String> list = new ArrayList<>();
+		if (needPagePrev) {
+			list.add("<<");
+		}
+		if (needPrev) {
+			list.add("<");
+		}
+		for (int i = startNavi; i <= endNavi; i++) {
+
+			list.add(String.valueOf(i));
+
+		}
+		if (needNext) {
+			list.add(">");
+		}
+		if (needPageNext) {
+			list.add(">>");
+		}
+
+		return list;
+
+	}
+	public int getRecordCount() throws Exception {
+		String sql = "select count(*) from board";
+		try (Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				ResultSet rs = pstat.executeQuery();) {
+			rs.next();
+			return rs.getInt(1);
+
+		}
+	}
 }
