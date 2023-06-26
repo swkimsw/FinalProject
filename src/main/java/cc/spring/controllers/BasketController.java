ab8e6d6212d03a19d2a9380c3177ece7a697fa48
이가은 <dmsalsgml84@naver.com>
2023-06-26 11:33:31 +0900

    BasketController add

---
+++ src/main/java/cc/spring/controllers/BasketController.java
@@ -0,0 +1,10 @@
+package cc.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basket/")
+public class BasketController {
+	
+}
