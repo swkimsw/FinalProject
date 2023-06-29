82ceb38f040c68d96a097dd073672de0f536a25d
soobinS2 <ysb930901@gmail.com>
2023-06-29 09:41:02 +0900

    aaa

--- src/main/java/cc/spring/controllers/AdminController.java
+++ src/main/java/cc/spring/controllers/AdminController.java
@@ -3,16 +3,15 @@ package cc.spring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import cc.spring.dto.MemberDTO;
 import cc.spring.services.AdminMemberService;
 
-@Controller
-@RequestMapping("/adminPage/")
+@RestController
+@RequestMapping("/data/")
 public class AdminController {
 	
 	@Autowired
