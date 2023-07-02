538e61933a40dee8e1e0a620838af34ce6991e04
SHK <lemona31@uos.ac.kr>
2023-07-03 08:54:42 +0900

    LoginValidator added

--- src/main/java/cc/spring/interceptors/LoginValidator.java
+++ src/main/java/cc/spring/interceptors/LoginValidator.java
@@ -1,5 +1,29 @@
 package cc.spring.interceptors;
 
-public class LoginValidator {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginValidator implements HandlerInterceptor{
+	
+	@Autowired
+	private HttpSession session;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+	
+		Integer code = (Integer)session.getAttribute("code");
+		if(code!=null) {
+			return true;
+		}else {
+			response.sendRedirect("/error");
+			return false;
+		}
+		
+	}
 
 }
