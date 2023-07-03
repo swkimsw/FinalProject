da60e491c3e7fdfd8be0b05dc90fbaf19afcf18e
agji12 <a@b>
2023-07-03 22:42:44 +0900

    Shop 코드 분리 완료

--- src/main/webapp/resources/js/shop/shopApply.js
+++ src/main/webapp/resources/js/shop/shopApply.js
@@ -1,3 +1,57 @@
+// 삭제, 취소 버튼 없애고 수정 완료, 취소 버튼 추가
+$("#updateBtn").on("click", function(){
+	$("#title").removeAttr("readonly");
+	$("#productName").removeAttr("readonly");
+	$("#productPrice").removeAttr("readonly");
+	$("#deadLineTemp").removeAttr("readonly");
+	$("#min").removeAttr("readonly");
+	$("#detail").removeAttr("readonly");
+	$("#imageSelect").removeAttr("style");
+	$("#imageSelect").attr("style", 'float:none; margin: 0 auto;');
+	$("#shippingCompany").removeAttr("readonly");
+	$("#shippingCompany").attr("class", 'memberInfo form-control');
+	$("#quantity").prop("readonly",true);
+		
+	$("#updateBtn, #deleteBtn, #back").css("display", "none");
+
+	let updateComplete = $("<button id='updateCompleteBtn' class='btn btn-success'>");
+	updateComplete.text("수정 완료");
+		
+	let cancel = $("<button type='button' class='btn btn-success'>");
+	cancel.text("취소");
+	cancel.on("click", function(){
+		location.reload();
+	})
+		
+	$(".buttons").append(updateComplete);
+	$(".buttons").append("&nbsp;");
+	$(".buttons").append(cancel);
+})
+
+// 공구 수정 완료 버튼 클릭시 유효성 검사
+$(document).on("click", "#updateCompleteBtn", function(){
+			
+	let regexProductPrice = /^[0-9]+$/;
+	let regexMin = /^[0-9]+$/;
+			
+	let productPrice = $("#productPrice").val();
+	let min = $("#min").val();
+			
+	let resultProductPrice = regexProductPrice.exec(productPrice);
+	let resultMin = regexMin.exec(min);
+			
+	if(!resultProductPrice){
+		alert("판매 가격 은 숫자로 입력해 주세요!");
+		return false;
+	}
+			
+	if(!resultMin){
+		alert("최소 수량 은 숫자로 입력해 주세요!");
+		return false;
+	}
+			
+})
+
 		
 // 답글 달기 버튼 눌렀을 때
 function viewInsertAnswer(code) {
