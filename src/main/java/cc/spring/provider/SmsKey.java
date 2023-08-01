9f88b7fc7de31922fda34315440f25a5bb5444cd
sinichiee <123343936+sinichiee@users.noreply.github.com>
2023-08-01 19:29:10 +0900

    Delete SmsKey.java

--- src/main/java/cc/spring/provider/SmsKey.java
+++
@@ -1,6 +0,0 @@
-package cc.spring.provider;
-
-public class SmsKey {
-	public static final String ACCESS_KEY = "voiNBakRVKkIuGp9UCgY";
-	public static final String SECRET_KEY = "2tyXlaQb9hOMWNhyfba3OocYqFdMOtOpeveuXvpw";
-}
